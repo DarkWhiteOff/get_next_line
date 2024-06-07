c0acbbdd574574636cdf32dac3412d11cc3a8dc6
Ziggy Amgar <zamgar@paul-f4Ar5s5.clusters.42paris.fr>
2024-06-07 18:29:50 +0200

    1

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zamgar <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 12:44:13 by zamgar            #+#    #+#             */
+/*   Updated: 2024/06/07 18:26:45 by zamgar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+#include <fcntl.h>
 
 char	*get_next_line(int fd)
 {
+	static char	buffer[BUFFER_SIZE];
+	char	*newLine;
+	int	nbRead;
+	static int	i;
+	int	count;
+	int	z;
 
+// while (buffer[BUFFER_SIZE] a encore de la place qui entoure tout
+	count = 0;
+	if (buffer[i - 1] == '\n')
+	{
+		buffer[i - 1] = '\0';
+		i--;
+	}
+	z = i;
+	while (buffer[i - 1] != '\n')
+	{
+		nbRead = read(fd, &buffer[i], 1);
+		if (nbRead == -1 ||  nbRead < 0)
+			return (NULL);
+		if (nbRead == 0)
+			return (NULL);
+		count++;
+		i++;
+	}
+	printf("Full buffer : %s", buffer);
+	newLine = (char *)malloc(sizeof(char) * count);
+	count = 0;
+	while (buffer[z] != '\0')
+	{
+		newLine[count] = buffer[z];
+		count++;
+		z++;
+	}
+	newLine[count] = '\0';
+	return (newLine);
+}
+
+int	main(void)
+{
+	int	fd = open("test.c", O_RDONLY);
+	int	i;
+	int	lineNb;
+
+	lineNb = 1;
+	while (i < 6)
+	{
+		printf("Line %d : %s\n\n", lineNb, get_next_line(fd));
+		i++;
+		lineNb++;
+	}
+	close(fd);
+	return (0);
 }
--- get_next_utils.c
+++ get_next_utils.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zamgar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/07 12:44:18 by zamgar            #+#    #+#             */
-/*   Updated: 2024/06/07 14:50:52 by zamgar           ###   ########.fr       */
+/*   Created: 2024/06/07 12:53:30 by zamgar            #+#    #+#             */
+/*   Updated: 2024/06/07 12:53:46 by zamgar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 42
-#endif
-
-char	*get_next_line(int fd);
-
-#endif
+#include "get_next_line.h"
